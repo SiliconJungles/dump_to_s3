ac7bffb4c4cc21621256dd0512f6e85e3aa6e937
yaojie <limyaojie93@gmail.com>
2019-10-11 17:07:24 +0800

    allow optional password and port

--- run.sh
+++ run.sh
@@ -32,26 +32,26 @@ if [ -z "${POSTGRES_ENV_POSTGRES_USER}" ]; then
   exit 1
 fi
 
-if [ -z "${POSTGRES_ENV_POSTGRES_PASSWORD}" ]; then
-  echo "You need to set the POSTGRES_ENV_POSTGRES_PASS environment variable."
-  exit 1
-fi
-
 if [ -z "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
   echo "You need to set the POSTGRES_PORT_5432_TCP_ADDR environment variable or link to a container named POSTGRES."
   exit 1
 fi
 
 if [ -z "${POSTGRES_PORT_5432_TCP_PORT}" ]; then
-  echo "You need to set the POSTGRES_PORT_5432_TCP_PORT environment variable or link to a container named POSTGRES."
-  exit 1
+  echo "POSTGRES_PORT_5432_TCP_PORT not set, defaulting to 5432"
+  POSTGRES_PORT_5432_TCP_PORT=5432
+fi
+
+
+if [ -z "${POSTGRES_ENV_POSTGRES_PASSWORD}" ]; then
+  echo "POSTGRES_ENV_POSTGRES_PASSWORD not set, proceeding without a password"
+else
+  export PGPASSWORD=${POSTGRES_ENV_POSTGRES_PASSWORD}
 fi
 
 POSTGRES_HOST_OPTS="-h $POSTGRES_PORT_5432_TCP_ADDR -p $POSTGRES_PORT_5432_TCP_PORT -U $POSTGRES_ENV_POSTGRES_USER"
 
-echo "Starting dump of ${PGDUMP_DATABASE} database(s) from ${POSTGRES_PORT_5432_TCP_ADDR}..."
-
-export PGPASSWORD=${POSTGRES_ENV_POSTGRES_PASSWORD}
+echo "Starting dump of ${PGDUMP_DATABASE} database from ${POSTGRES_PORT_5432_TCP_ADDR}..."
 
 DUMPFILE="${PGDUMP_DATABASE}_$(date +%d-%b-%Y-%H-%M-%S).sql"
 pg_dump $PGDUMP_OPTIONS $POSTGRES_HOST_OPTS $PGDUMP_DATABASE > $DUMPFILE
